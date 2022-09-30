7a679788f38dc823f27837a0e43a902f6feb3c69
Sai Ravi Varma Pallaprolu <sai.pallaprolu@wipro.com>
2022-09-30 11:34:22 +0530

    contact Page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import ContactUs from './ContactUs';
+import Welcome from './Welcome';
+import Header from './Header';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   // var image = ;
   return (
     <div className="App">
-      <div className="header">
-          <h1>Sai's Space</h1>
-          <div className="headerRight">
-              <h1>About</h1>
-              <h1>Contact</h1>
-          </div>
-      </div>
-      <div className="image">
-          <div className='leftSide'>
-                <h1 >Hey Buddy</h1>
-                <h2>How are you. Searching for great deals????</h2>
-                <h3>You're at right place &#128540;</h3>
-                <button>Go Home</button>
-          </div>
-           <div className="minions"/>
-      </div>
+      <BrowserRouter>
+          <Header/>
+          <Routes>
+            <Route path='/' element={<Welcome/>}></Route>
+            <Route path='/contactUs' element={<ContactUs/>}></Route>
+          </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/ContactUs.js
@@ -0,0 +1,68 @@
+import "./ContactUs.css"
+import styled from 'styled-components'
+
+const ContactUs = () => {
+    const MainContainer = styled.div`
+
+    display: "flex" ;
+    align-items:center;
+    flex-direction:column;
+    height:70vh;
+    width:70vw;
+    background: linear-gradient(
+        120deg,
+        rgba(255, 255, 255, .25),
+        rgba(255, 255, 255, .25), 70%, 
+        rgba(255, 255, 255, .25)
+      );
+    border-radius: 20px;
+    border: 1px solid rgba(255, 255, 255, .25);
+    box-shadow:
+    inset 4px 4px 5px 0 rgba(255, 255, 255, .25),
+    inset -4px -4px 5px 0 rgba(0, 0, 0, 0.125),
+    0 0 10px 1px rgba(0, 0, 0, 0.25);
+}
+backdrop-filter:
+blur(15px)
+saturate(100%);
+    `;
+    return (
+        <div className="contactUs">
+                <MainContainer>
+                    <div className="top">
+                            <h1>Anything wrong??</h1>
+                    </div>
+                    <div className="main">
+                        <div className="leftInContactUs">
+                                <p>Helloooo</p>
+                                <h1>I'm Sai</h1>
+                                <h3>I'm here to help you.....</h3>
+                                <h3>Share your details we will be back to you</h3>
+                                <div className="contactMinions"></div>
+                                <div className="minionSmall"></div>
+                        </div>
+                        <div className="rightInContactUs">
+                            <form>
+                               <p>Name</p>
+                               <input type='text' placeholder="Name..."></input>
+                               <p>Mobile</p>
+                               <input type='text' placeholder="Mobile..."></input>
+                               <p>email</p>
+                               <input type='text' placeholder="Email..."></input>
+                               <input type='button' value="Send"></input>
+                            </form>
+                            <div className="drops">
+                                <div className="drop drop-1"></div>
+                                <div className="drop drop-2"></div>
+                                <div className="drop drop-3"></div>
+                                <div className="drop drop-4"></div>
+                                <div className="drop drop-5"></div>
+                            </div>
+                        </div>
+                    </div>
+                </MainContainer>
+        </div>
+    );
+}
+
+export default ContactUs;
